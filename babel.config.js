91188a0e1bf3e2425f924aa95323790bf29aa307
waello <jlassiwael@gmail.com>
2022-02-24 00:05:43 +0100

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  ]
 }
